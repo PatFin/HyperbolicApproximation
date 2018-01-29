33b4ec658b75bf07ed85599cbb680f319784a0a0
Patrick Finnerty <patrick.finnerty@insa-lyon.fr>
2018-01-29 13:36:34 +0900

    Matlab function performing the numerical approximation

---
+++ Approximate.m
@@ -0,0 +1,38 @@
+function [P] = Approximate(a, b, lambda, d, N)
+
+%Creating our discrete domain as matrix P
+%   As we are using matrices for a triangular domain there will be some
+%   waste
+P = zeros(N,N);
+
+%Applying the boundary conditions
+%   Setting the right column to 0
+P(:,N)=0;
+%   Applying the diagonal condition
+for i = 1:N
+    x = a + ((b-a)/N)*i;
+    P(i,i)= (1/2)*(lambda + d)*(x - b);
+end
+
+% handy constant values to keep expressions short in the following 
+% approximations
+h = (b-a)/N;
+v = -(lambda + d);
+
+% Going through the whole domain to compute the next approximation for each
+% cell of the matrix. We do not touch the areas whose boundary conditions 
+% was set.
+for j = N-2:-1:1
+    for i = N-1:-1:j+1
+        if i == j+1
+            % Points directly below the diagonal
+            P(j,i) = (P(j,i+1) + P(j,i-1))/2;
+        else
+            % Regular approximation formula
+            P(j,i) = (P(j+1,i+1) + P(j+1,i-1) - P(j+2,i))-v*h*h*P(j+1,i);
+        end
+    end
+end
+
+end
+
